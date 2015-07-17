35a6b0de9b3d8b5a6c3de227b23f2bd6832fb097
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-17 16:38:57 +0900

    serial test

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,72 +1,20 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"strconv"
-	"strings"
 
-	"github.com/huin/goserial"
+	"github.com/mikepb/go-serial"
 )
 
-type packet struct {
-	header     string
-	coord      string
-	dataR      string
-	dataG      string
-	terminator string
-}
-
-func getUsbttyList() []string {
-	contents, _ := ioutil.ReadDir("/dev")
-	var ret []string
-
-	for _, f := range contents {
-		if strings.Contains(f.Name(), "tty.usb") ||
-			strings.Contains(f.Name(), "ttyUSB") {
-			//return "/dev/" + f.Name()
-			ret = append(ret, "/dev/"+f.Name())
-		}
-	}
-
-	return ret
-}
-
-func ttySelecter(ttys []string) (string, error) {
-	for i, s := range ttys {
-		fmt.Println(strconv.Itoa(i) + ": " + s)
-	}
-	fmt.Println()
-	fmt.Print("Select tty prot: ")
-
-	n := 0
-	fmt.Scan(&n)
-
-	if n > len(ttys)-1 {
-		return "", errors.New("tty port select error. " + strconv.Itoa(n) + " is not exist.")
-	}
-
-	return ttys[n], nil
-}
-
-func viewTtySelecterUI() (string, error) {
-	ttys := getUsbttyList()
-	tty, err := ttySelecter(ttys)
-
-	fmt.Println(tty)
-	return tty, err
-}
-
 func main() {
-	ttyPort, err := viewTtySelecterUI()
+	info, err := serial.ListPorts()
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	serialConfigure := &goserial.Config{Name: ttyPort, Baud: 9600}
-	serialPort, _ := goserial.OpenPort(serialConfigure)
-
-	fmt.Println(serialPort)
+	for _, p := range info {
+		fmt.Println(p.Name())
+	}
 }

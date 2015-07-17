3648e32fb5e9053b99096a24067a669eb2187ad2
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-18 00:35:39 +0900

    文字の画像への変換

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"errors"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
 	"io"
 	"io/ioutil"
 	"log"
 	"strconv"
 	"strings"
 
-	"github.com/huin/goserial"
+	"github.com/disintegration/imaging"
+
+	"code.google.com/p/freetype-go/freetype"
 )
 
 type packet struct {
@@ -99,18 +104,93 @@ func viewTtySelecterUI() (string, error) {
 	return tty, err
 }
 
-func main() {
-	ttyPort, err := viewTtySelecterUI()
+func convertString2image(s string) (*image.RGBA, error) {
+	dpi := float64(72.0)
+	fontfile := "../font/MS Gothic.ttf"
+	hinting := "none"
+	size := float64(17)
+	spacing := float64(0)
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(fontfile)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
+		return nil, err
+	}
+	font, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return nil, err
 	}
 
-	serialConfigure := &goserial.Config{Name: ttyPort, Baud: 9600}
-	serialPort, _ := goserial.OpenPort(serialConfigure)
+	// Initialize the context.
+	fg, bg := image.Black, image.White
+	rgba := image.NewRGBA(image.Rect(0, 0, 16, 16))
+	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	c := freetype.NewContext()
+	c.SetDPI(dpi)
+	c.SetFont(font)
+	c.SetFontSize(size)
+	c.SetClip(rgba.Bounds())
+	c.SetDst(rgba)
+	c.SetSrc(fg)
+	switch hinting {
+	default:
+		c.SetHinting(freetype.NoHinting)
+	case "full":
+		c.SetHinting(freetype.FullHinting)
+	}
 
-	packet := createTestPacket()
-	for {
+	// Draw the text.
+	pt := freetype.Pt(-1, -3+int(c.PointToFix32(size)>>8))
+
+	_, err = c.DrawString(s, pt)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	pt.Y += c.PointToFix32(size * spacing)
+
+	return rgba, nil
+}
+
+func cancellationAntiAliasing(img *image.RGBA) *image.NRGBA {
+	gray := imaging.Grayscale(img)
+	//imaging.Save(gray, "./grayscaled.png")
+	w := gray.Rect.Max.X
+	h := gray.Rect.Max.Y
+
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			r, _, _, _ := gray.At(x, y).RGBA()
+			if r > 27000 {
+				c := color.RGBA{0xff, 0xff, 0xff, 0xff}
+				gray.Set(x, y, c)
+			} else {
+				c := color.RGBA{0x00, 0x00, 0x00, 0xff}
+				gray.Set(x, y, c)
+			}
+		}
+	}
+	return gray
+}
+
+func main() {
+	/*
+		ttyPort, err := viewTtySelecterUI()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		serialConfigure := &goserial.Config{Name: ttyPort, Baud: 9600}
+		serialPort, _ := goserial.OpenPort(serialConfigure)
+
+		packet := createTestPacket()
 		writeLCDMatrix(packet, serialPort)
-	}
+	*/
+	//gray := imaging.Grayscale(convertString2image("A"))
+	img, _ := convertString2image("０")
+	gray := cancellationAntiAliasing(img)
 
+	imaging.Save(gray, "./grayscaled.png")
 }

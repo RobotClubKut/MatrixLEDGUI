02c98d2e78bb7eb19629cdf5f51f8af0e8835c66
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-18 03:11:59 +0900

    文字のエンコード周辺にバグが有るっぽい

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"image"
@@ -13,6 +14,7 @@ import (
 	"strings"
 
 	"github.com/disintegration/imaging"
+	"github.com/huin/goserial"
 
 	"code.google.com/p/freetype-go/freetype"
 )
@@ -32,28 +34,59 @@ type lcdChar struct {
 }
 
 type lcdString struct {
-	c         []lcdChar
-	drowCoord int
+	c     []lcdChar
+	coord int
 }
 
 type lcdMatrix struct {
-	data [96][16]int
+	dataR [3][16]uint32
+	dataG [3][16]uint32
 }
 
 func createTestPacket() *packet {
 	var p packet
 	p.header = "pcmat\r"
 	p.coord = "000\r5ff\r"
-	s := ""
+	s1 := ""
+	s2 := ""
 	for i := 0; i < 192; i++ {
-		s += "p"
+		s1 += "p"
+		s2 += "a"
 	}
-	s += "\r"
-	p.dataR = s
-	p.dataG = s
+	s1 += "\r"
+	s2 += "\r"
+	p.dataR = s1
+	p.dataG = s2
 	p.terminator = "end\r"
 
 	return &p
+}
+
+func createPacket(str lcdString) *packet {
+	data, _ := printLCD(str, 0)
+
+	packet := createTestPacket()
+	var bufr []byte
+	var bufg []byte
+	for y := 0; y < 16; y++ {
+
+		for x := 0; x < 3; x++ {
+			r := data.dataR[x][y]
+			binr := make([]byte, 4)
+			binary.LittleEndian.PutUint32(binr, r)
+			bufr = append(bufr, binr...)
+
+			g := data.dataR[x][y]
+			bing := make([]byte, 4)
+			binary.LittleEndian.PutUint32(bing, g)
+			bufg = append(bufg, bing...)
+		}
+
+	}
+	packet.dataR = string(bufr) + "\r"
+	packet.dataG = string(bufg) + "\r"
+
+	return packet
 }
 
 func getUsbttyList() []string {
@@ -175,22 +208,118 @@ func cancellationAntiAliasing(img *image.RGBA) *image.NRGBA {
 	return gray
 }
 
-func main() {
-	/*
-		ttyPort, err := viewTtySelecterUI()
-		if err != nil {
-			log.Fatalln(err)
+func convertLCDChar(c string, color int) *lcdChar {
+	image, _ := convertString2image(c)
+	img := cancellationAntiAliasing(image)
+
+	var ret lcdChar
+	ret.color = color
+	ret.c = c
+
+	for y := 0; y < 16; y++ {
+		ret.bitmap[y] = 0
+		for x := 0; x < 16; x++ {
+			ret.bitmap[y] = ret.bitmap[y] << 1
+			r, _, _, _ := img.At(x, y).RGBA()
+			if r == 0 {
+				ret.bitmap[y] |= 1
+			} else {
+				ret.bitmap[y] |= 0
+			}
 		}
+	}
+	return &ret
+}
 
-		serialConfigure := &goserial.Config{Name: ttyPort, Baud: 9600}
-		serialPort, _ := goserial.OpenPort(serialConfigure)
+func convertLCDString(str string, color int) *lcdString {
+	var ret lcdString
 
-		packet := createTestPacket()
+	for _, c := range str {
+		ret.c = append(ret.c, *convertLCDChar(string(c), color))
+	}
+	ret.coord = 0
+	return &ret
+}
+
+func connectLCDStr(str0 *lcdString, str1 *lcdString) *lcdString {
+	var ret lcdString
+	ret.c = append(ret.c, str0.c...)
+	ret.c = append(ret.c, str1.c...)
+	ret.coord = 0
+	return &ret
+}
+
+func printLCD(str lcdString, shift int) (*lcdMatrix, *lcdString) {
+	var ret lcdMatrix
+
+	for y := 0; y < 16; y++ {
+		var bufR []uint8
+		var bufG []uint8
+		for cn := 0; cn < len(str.c); cn++ {
+			for x := 0; x < 16; x++ {
+				bit := str.c[cn].bitmap[y]
+				bit = bit >> (15 - uint(x))
+				//mask 0b0000000000000001
+				bit = bit & 0x0001
+				//赤があるとき
+				//buf[coordX] = uint8(bit)
+				if str.c[cn].color == 0xff0000 {
+					bufR = append(bufR, uint8(bit))
+				}
+				if str.c[cn].color == 0x00ff00 {
+					bufG = append(bufG, uint8(bit))
+				}
+			}
+		}
+		i := 0
+		counter := 0
+
+		for x := 0; x < 96; x++ {
+			if counter == 32 {
+				i++
+				if i == 3 {
+					break
+				}
+				counter = 0
+			}
+			ret.dataR[i][y] = ret.dataR[i][y] << 1
+			if str.coord+x < len(bufR) {
+				ret.dataR[i][y] |= uint32(bufR[str.coord+x])
+			} else {
+				ret.dataR[i][y] |= 0
+			}
+			ret.dataG[i][y] = ret.dataG[i][y] << 1
+			if str.coord+x < len(bufG) {
+				ret.dataG[i][y] |= uint32(bufG[str.coord+x])
+			} else {
+				ret.dataG[i][y] |= 0
+			}
+			counter++
+		}
+	}
+	str.coord += shift
+	fmt.Println(ret)
+	return &ret, &str
+
+}
+
+func main() {
+
+	str := convertLCDString("a", 0x00ff00)
+	packet := createPacket(*str)
+
+	ttyPort, err := viewTtySelecterUI()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	serialConfigure := &goserial.Config{Name: ttyPort, Baud: 9600}
+	serialPort, _ := goserial.OpenPort(serialConfigure)
+
+	packet = createTestPacket()
+	for i := 0; i < 100; i++ {
 		writeLCDMatrix(packet, serialPort)
-	*/
+	}
 	//gray := imaging.Grayscale(convertString2image("A"))
-	img, _ := convertString2image("０")
-	gray := cancellationAntiAliasing(img)
-
-	imaging.Save(gray, "./grayscaled.png")
+	//imaging.Save(gray, "./grayscaled.png")
 }

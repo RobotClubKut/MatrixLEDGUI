9570bbd0cf9335017634de7c73a2f3568a100775
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-20 21:56:27 +0900

    戻した

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -78,10 +78,10 @@ func compressMatrixChar(c string, font string, color uint32) *MatrixCharData {
 		b = (bitmap & 0x000000ff) >> 0
 		buf = append(buf, byte(b))
 		//圧縮して押し込む
-		//ch := make(chan []byte)
-		//go compressChar("none", buf, ch)
-		//ret.Bitmap[y] = <-ch
-		ret.Bitmap[y] = buf
+		ch := make(chan []byte)
+		go compressChar("none", buf, ch)
+		ret.Bitmap[y] = <-ch
+		//ret.Bitmap[y] = buf
 	}
 	return &ret
 }
@@ -90,8 +90,8 @@ func ReadMatrixChar(cm MatrixCharData) *MatrixChar {
 	var ret MatrixChar
 	ret.Color = cm.Color
 	for y := 0; y < 16; y++ {
-		//buf := uncompressChar("none", cm.Bitmap[y])
-		buf := cm.Bitmap[y]
+		buf := uncompressChar("none", cm.Bitmap[y])
+		//buf := cm.Bitmap[y]
 		ret.Bitmap[y] = 0
 		fin := make(chan bool)
 		go func() {

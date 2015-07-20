500b6f832b83802fe365249a4eaa1396aefa1385
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-20 19:21:43 +0900

    通信周り

--- go/career/career.go
+++ go/career/career.go
@@ -1,1 +1,51 @@
 package career
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func getUsbttyList() []string {
+	contents, _ := ioutil.ReadDir("/dev")
+	var ret []string
+
+	for _, f := range contents {
+		if strings.Contains(f.Name(), "tty.usb") ||
+			strings.Contains(f.Name(), "ttyUSB") ||
+			strings.Contains(f.Name(), "ttyACM") {
+			//return "/dev/" + f.Name()
+			ret = append(ret, "/dev/"+f.Name())
+		}
+	}
+
+	return ret
+}
+
+func ttySelecter(ttys []string) (string, error) {
+	for i, s := range ttys {
+		fmt.Println(strconv.Itoa(i) + ": " + s)
+	}
+	fmt.Println()
+	fmt.Print("Select tty prot: ")
+
+	n := 0
+	fmt.Scan(&n)
+
+	if n > len(ttys)-1 {
+		return "", errors.New("tty port select error. " + strconv.Itoa(n) + " is not exist.")
+	}
+
+	return ttys[n], nil
+}
+
+// ViewTtySelecterUI UI Viewer
+func ViewTtySelecterUI() (string, error) {
+	ttys := getUsbttyList()
+	tty, err := ttySelecter(ttys)
+
+	fmt.Println(tty)
+	return tty, err
+}

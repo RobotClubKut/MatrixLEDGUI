8a642021e8992094f34c0955c2064f40c4cc8fa2
shirase-aoi <ieee0824@ezweb.ne.jp>
2015-07-20 19:48:10 +0900

    fontSelector

---
+++ go/font/font.go
@@ -0,0 +1,27 @@
+package font
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func SelectFont() (string, error) {
+	fontDir := "../font/"
+	list, err := ioutil.ReadDir(fontDir)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+	for i, f := range list {
+		fmt.Printf("%d: ", i)
+		fmt.Println(f.Name())
+	}
+
+	n := 0
+	fmt.Println()
+	fmt.Print("Select font file: ")
+	fmt.Scan(&n)
+	fmt.Println()
+	return "../font/" + list[n].Name(), nil
+}
